66b6867efcb4fc75773597641efb5894408e926f
ryan-kobayashi <rkobayashi@wesleyan.edu>
2023-11-15 01:08:58 -0500

    completed the signup form checkers for front end

--- src/screens/LogInScreen.js
+++ src/screens/LogInScreen.js
@@ -1,8 +1,7 @@
 import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import React, { useState } from "react";
 import InputField from "../components/InputField";
-import SubmitButton from "../components/SubmitButton";
-import axios from 'axios';
+import axios from "axios";
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
@@ -10,29 +9,31 @@ const LoginScreen = ({ navigation }) => {
 
   const handleSubmit = () => {
     axios
-      .post("http://192.168.4.26:8080/index.php/user/login",
-      JSON.stringify({
-        email: email,
-        password: password,
-      }))
+      .post(
+        "http://172.21.55.39:8080/index.php/user/login",
+        JSON.stringify({
+          email: email,
+          password: password,
+        })
+      )
       .then((response) => {
         if (response.status === 200) {
-          console.log("login successful", response)
+          console.log("login successful", response);
           // if (response.headers.authorization) {
           //   // get authorization header from response
           //   // get token from authorization header
           //   // store token in storage for future authenticated requests
           //   // i.e., AsyncStorage.setItem('token', token);
           // }
-          navigation.navigate("reviews")
+          navigation.navigate("reviews");
         }
       })
       .catch((error) => {
         if (error.response.status === 401) {
-          console.log('incorrect credentials')
+          console.log("incorrect credentials");
         } else if (error.response.status === 404) {
-          console.log('account does not exist', error);
-          navigation.navigate('sign up');
+          console.log("account does not exist", error);
+          navigation.navigate("sign up");
         }
       });
   };
@@ -55,6 +56,7 @@ const LoginScreen = ({ navigation }) => {
         required
       />
       <Button title="login" onPress={handleSubmit} />
+
       <TouchableOpacity onPress={() => navigation.navigate("sign up")}>
         <Text style={styles.tologin}>
           don't have an account? click here to create one!
@@ -66,11 +68,12 @@ const LoginScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: "70%",
-    margin: 10,
-    marginTop: "70%",
+    padding: 10,
+    paddingTop: "70%",
+    paddingBottom: "100%",
     alignItems: "center",
-    marginHorizontal: "15%",
+    paddingHorizontal: "15%",
+    backgroundColor: "#d7dedc",
   },
   title: {
     fontSize: 30,
